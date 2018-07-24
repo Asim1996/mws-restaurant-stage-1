b4856cfb4762f9509d510b5f3bc5f06bab797e65
Asim1996 <awesomeasim2596@gmail.com>
2018-07-25 01:33:14 +0530

    feat:Fetch complete Replace XHR with fetch to get data from a development server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -3,38 +3,37 @@
  */
 class DBHelper {
 
-  /**
-   * Database URL.
-   * Change this to restaurants.json file location on your server.
-   */
-  static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    /*
+      Development Sever
+    */
+    static get DATABASE_URL() {
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
+
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
+    fetch(DBHelper.DATABASE_URL)
+    .then((response) =>{
+      if(!response.ok){
+        throw response.statusText;
       }
-    };
-    xhr.send();
+      return response.json();
+    }).then((data) =>{
+        const restaurants = data;
+        callback(null, restaurants);
+    }).catch((error) => {
+        callback(error, null); 
+    })
   }
-
+    
   /**
    * Fetch a restaurant by its ID.
    */
-  static fetchRestaurantById(id, callback) {
+    static fetchRestaurantById(id, callback) {
     // fetch all restaurants with proper error handling.
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
